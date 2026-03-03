6d62288e29ced61b1fcc8313b04883ef25effea8
Rampreet Kumar <rkofficial1st@gmail.com>
2026-03-03 14:55:04 +0530

    me

---
+++ 03_form/src/components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaEye } from "react-icons/fa";
+
+const Form = () => {
+    return (
+
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "90vh"
+        }}>
+            <div style={{fontSize: "40px"}}><FaEye /></div>
+        </div>
+    )
+}
+
+export default Form
