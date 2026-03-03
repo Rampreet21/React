037c0188aee0468211f61ad5d75693175bdc5174
Rampreet Kumar <rkofficial1st@gmail.com>
2026-03-03 21:17:03 +0530

    me

--- 03_form/src/components/Form.jsx
+++ 03_form/src/components/Form.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import { FaEye } from "react-icons/fa";
+import { useState } from 'react';
+import '../style/Form.css'
 
 const Form = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [showP, setShowP] = useState(false);
+
+
+    function subhandle(e) {
+        e.preventDefault();
+        console.log({name,email,pass});
+    }
+
+    console.log
     return (
 
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "90vh"
-        }}>
-            <div style={{fontSize: "40px"}}><FaEye /></div>
-        </div>
+        <form className='form'>
+            <h2>SIGN UP</h2>
+            <label className='label' htmlFor="">NAME</label>
+            <input onChange={(e) => {
+                setName(e.target.value)
+            }} className='inputbox' type="text" value={name} />
+            <label className='label' htmlFor="">EMAIL</label>
+            <input onChange={(e) => { setEmail(e.target.value) }} className='inputbox' type="text" value={email} />
+            <label className='label' htmlFor="">PASSWORD</label>
+            <div className='pdiv'>
+            <input onChange={(e) => { setPass(e.target.value) }} className='inputbox' type= {showP? "text":"password"} />
+            <span onClick={()=>{setShowP(!showP)}}>show</span>
+            </div>
+            <button onClick={subhandle}> SUBMIT</button>
+        </form>
     )
 }
 
